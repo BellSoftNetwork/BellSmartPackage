5509c6743375d67902a9e438e349a32439fb00d3
방울 <bell04204@gmail.com>
2015-06-04 22:08:37 +0900

    BSS 프로젝트 추가 (2 솔루션)

---
+++ Bell Smart Server/Source/BSS/BSS_Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bell_Smart_Server.Source.BSS
+{
+    public partial class BSS_Main : Form
+    {
+        public BSS_Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
