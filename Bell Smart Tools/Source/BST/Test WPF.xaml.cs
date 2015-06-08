e00ebde4b0636a71b2571f82c4b8524ab233566f
유지수 <det3652000@naver.com>
2015-06-09 01:35:13 +0900

    되는겨 마는겨

--- Bell Smart Tools/Source/BST/Test WPF.xaml.cs
+++ Bell Smart Tools/Source/BST/Test WPF.xaml.cs
@@ -10,13 +10,15 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
 
 namespace Bell_Smart_Tools.Source.BST
 {
     /// <summary>
     /// Test_WPF.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class Test_WPF : Window
+    public partial class Test_WPF : MetroWindow
     {
         public Test_WPF()
         {
